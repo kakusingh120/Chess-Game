739acd9045164c297457a0c29bcdf82f661cfd47
krish singh <krishsingh10386@gmail.com>
2025-04-04 10:49:50 +0530

    update app js file

--- app.js
+++ app.js
@@ -86,6 +86,6 @@ app.get('/', (req, res) => {
     res.render('index', { title: "Chess game" });
 })
 
-server.listen(8080, () => {
-    console.log(`Server is listening on port http://localhost:8080`);
+server.listen(process.env.PORT || 8088 , () => {
+    console.log(`Server is listening on port http://localhost:8088`);
 });
